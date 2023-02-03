9db67857c99eba3676b60be14f7ce79132f7eb6e
Lambvard <afolabi_babatunde18@yahoo.com>
2023-02-04 00:03:25 +0100

    Index

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+  <meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1">
+<title>DUFIL::FORMS</title>	
+<link rel="stylesheet" type="text/css" href="boots/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="boots/css/mine.css">
+<link rel="stylesheet" type="text/css" href="boots/fonts/Fonts/css/all.css">
+<script type="text/javascript" src="boots/jquery-3.5.1.min.js"></script>
+
+
+
+<script type="text/javascript">
+  $(document).ready(function(){
+
+    //var userfeedback=alert('Hurray! \nWe are ready to beautify our Paperless Forms site more than before with a better and more user-friendly interface and with access directly from the internet. In few days, you will experience the transition from the old design to the new one.  \n\n -Dufil IT Service Management');
+ });
+</script>
+
+
+</head>
+
+<body>
+<!--firstHeader-->
+
+
+<section class="navbar fixed-top col-sm-12" style="background-color: #4B026D; height: 50px;" >
+    <div  class ="container">
+      <div class="row col-sm-12">
+          <div class="col-sm-2"><a class="navbar-brand" href="#"><img src="image/newlogo.png" class="form-inline align top" alt="" style="height:100px; width:150px; padding-bottom: 70px;"></a>
+      </div>
+<div class="col-sm-6" style="color: white;">
+  
+<div class="row col-sm-12" style="">
+      <div class="col-sm-2">
+        <img src="Image/DufPrima.png" width="50px">
+      </div>
+      <div class="col-sm-10">
+        <nav class="nav" style="font-size: 18px; font-weight: bold; color: white;">
+          <div class="nav-item" style="margin-right: 30px;">
+            <a href="#" style="text-decoration: none;color: red;">Home</a>
+          </div>
+          <div class="nav-item" style="margin-right: 30px;">
+           <a href="resource/covid.php" style="text-decoration: none;color: white;">About Covid</a> 
+          </div>
+
+          <div class="nav-item" style="margin-right: 30px;">
+            <a href="#" style="text-decoration: none;color: white;">feedback</a>
+          </div>
+        </nav>
+      </div>
+      
+    </div><!--This the end of the main wrapper -->
+
+
+
+
+
+
+</div>
+<div class="col-sm-4 col-lg-4 col-md-4">
+<form class="form-inline">
+  <input class="form-control " type="search" placeholder="Search Google" aria-label="Search" style="margin-bottom: 0px;">
+  <button class="btn btn my-2 my-sm-0" type="submit" style="margin-top: 10px; margin-bottom: 10px; background-color: #e70917; color: white;" >Search</button>
+</form>
+
+</div>
+      </div><!--Shrinker -->
+
+</div><!--central collator -->
+
+   
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <section class="containerm2" style="margin-top: 10px; ">
+
+    <header class="navbar fixed-top navbar-light" style=" margin-top: 51px; background-color: #f8f9fa;">
+    <div style="margin: auto; width: 1250px;">
+        </div>
+  </header>      
+  </section>
+<!-- The body-->
+  <section >
+    <div class="bodyimage" style="height: 550px;">
+      
+    
+  </div>
+
+    
+    </div>
+    <div class="row">
+    <div style="margin: auto;margin-top: -340px; width: 980px; margin-bottom: 200px;">
+      <div align="center"><label style="color: white; font-size: 50px;">Paperless without boundary!</label></div>
+      <form class="form-inline" target="_blank" action="http://google.com">
+        <div style="width: 980px;" align="center"><input type="text" name="" class="form-control form-control-add" placeholder="Search your the world without boundary" style="width:600px;"><input type="submit" name="" value="Search your mind" class="btn btn- form-control-add" style="background-color: #e70917; color: white; margin-left: 3px; width: 170px;" required></div>
+      </form>
+    </div>
+  </section>
+
+
+    <div class="container-fluid" align="center">
+      <div class="row">
+      <div class="col-sm-4 col-xl-4 col-md-4 col-lg-4" style="color: #4B026D; font-size: 180px;" ><i class="fas fa-viruses"></i>
+          <div align="center" style="font-size: 25px; font-weight: bold; "><a href="resource/Covid.php" style="text-decoration: none; color: ">What is Covid?</a></div>
+      </div>
+      <div class="col-sm-4 col-xl-4 col-md-4 col-lg-4 " style="color: green;font-size: 180px;"><i class="fas fa-user-cog"></i>
+        <div align="center" style="font-size: 25px; font-weight: bold; color: #4B026D;"><a href="#"  style="text-decoration: none; color: ">Feedback</a></div>
+      </div>
+      <div class="col-sm-4 col-xl-4 col-md-4 col-lg-4 " style="font-size: 180px;margin-top: 0px; color:red;"><i class="fas fa-book-reader"></i>
+        <div align="center" style="font-size: 25px; font-weight: bold; color: #4B026D;"><a href="#"  style="text-decoration: none; color: ">FAQ</a></div>
+      </div>
+      </div>
+    </div>
+
+      <div align="center" style="margin-top: 30px;" ><label style="font-size: 50px; font-weight: bold; color:;">Choose Your Report!</label></div>
+    <div class="container-fluid" align="center">
+      <div class="row">
+        <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+          <a href="../IOU"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: #fcc635;"><i class="fas fa-book-reader" style="font-size: 80px;color: "></i><br><label style="font-size: 30px;color: black;">IOU</label></div></a>
+
+        </div>
+         <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+           <a href="../Sensoryuser"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: #001a4d;"><i class="fas fa-utensils" style="font-size: 80px;color: white;"></i><br><label style="font-size: 30px;color: white;">Corp Sensory</label></div></a>
+          </div>
+          <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+           <a href="../leave"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: white; border:1px solid black;"><i class="fab fa-wpforms" style="font-size: 80px;"></i><br><label style="font-size: 30px;">Leave Form</label></div></a>
+          </div>
+
+          <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+          <a href="../medics"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px; background-color: #4B026D; background: linear-gradient (#4B026D,white)"><i class="fab fa-medrt" style="font-size: 80px;color: white;"></i><br><label style="font-size: 30px;color:white;">Medical Form</label></div></a>
+          </div>
+
+      </div>
+
+
+      <div class="row">
+        <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+          <a href="../Liquidation"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: #fcc635;"><i class="fas fa-landmark" style="font-size: 80px;color: "></i><br><label style="font-size: 30px;color: black;">Liquidition</label></div></a>
+        </div>
+         <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+           <a href="../CAPA"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: #fb6b02;"><i class="fas fa-check-double" style="font-size: 80px;color: white;"></i><br><label style="font-size: 30px;color: white;">CAPA</label></div></a>
+          </div>
+          <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+           <a href="./loan"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: white; border:1px solid black;"><i class="fas fa-list" style="font-size: 80px;"></i><br><label style="font-size: 30px;">Loan Form</label></div></a>
+          </div>
+
+          <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+          <a href="../Poolcar"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px; background-color: #4B026D;"><i class="fa fa-bus" style="font-size: 80px;color: white;"></i><br><label style="font-size: 30px;color:white;">Pool Car</label></div></a>
+          </div>
+
+      </div>
+
+
+
+      <div class="row">
+        <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+          <a href="../petty"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: #fcc635;"><i class="fas fa-money-bill-wave" style="font-size: 80px;color: "></i><br><label style="font-size: 30px;color: black;">Petty Voucher</label></div></a>
+        </div>
+         <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+          <a href="../ious"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: #001a4d;"><i class="fa fa-database" aria-hidden="true" style="font-size: 80px;color: white;"></i><br><label style="font-size: 30px;color: white;">Liquidation(EXP)</label></div></a>
+          </div>
+
+          <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+           <a href="../Itemrequest"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px;background-color: white; border:1px solid black;"><i class="fas fa-solid fa-align-justify" style="font-size: 80px;"></i><br><label style="font-size: 30px;">Item Request Form</label></div></a>
+           </div>
+
+          <div class="col-sm-3 col-md-3 col-xl-3 col-lg-3">
+          <a href="Itgatepass/centralgatepass.php"><div class="jumbotron" align="center" style="padding:0px;padding-top: 20px;padding-right: 0px; background-color: #4B026D;"><i class="fa fa-briefcase" style="font-size: 80px;color: white;"></i><br><label style="font-size: 30px;color:white;">GATE PASS</label></div></a>
+          </div>
+
+      </div>
+
+      <!-- -->
+
+
+
+
+
+      </div>
+    
+
+  <section>
+    <div class="col-sm-12" align="center" style="font-weight: bold;"><label>Copyright &copy of Dufil ICT Team</label></div>
+  </section>
+
+  </body>
+
+
+    
